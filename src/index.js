b776f915b673c8cdd41705544935425643270511
mkinitcpio <aliaksandr.kazhamiaka@gmail.com>
2018-06-16 23:25:18 +0300

    Add config support

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('psb run');
+const fs = require('fs');
+const path = require('path');
+const defaultConfig = require('./default/psb.config');
+const psb = require('./psb');
+
+let config = null;
+const configName = 'psb.config';
+const isCustomConfigExist = fs.existsSync(configName);
+
+if (isCustomConfigExist) {
+    const workingDir = process.env.PWD;
+    config = require(path.join(workingDir, configName));
+} else {
+    config = defaultConfig;
+}
+
+try {
+    const arg = process.argv[2];
+    psb[arg](config);
+} catch {
+    throw new Error(`Unexpected argument: ${arg}`);
+}
---
+++ src/psb.js
@@ -0,0 +1,12 @@
+const build = (config) => {
+    console.log('build ->', config);
+}
+
+const pack = (config) => {
+    console.log('pack ->', config);
+}
+
+module.exports = {
+    build,
+    pack
+}
