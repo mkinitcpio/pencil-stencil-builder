cf436c8ae8be1d79da89f708d23c82a2c37e73fd
mkinitcpio <aliaksandr.kazhamiaka@gmail.com>
2018-06-17 01:00:09 +0300

    Add configs merge support

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ const fs = require('fs');
 const path = require('path');
 const defaultConfig = require('./default/psb.config');
 const psb = require('./psb');
+const merge = require('deepmerge');
 
 let config = null;
-const configName = 'psb.config';
+const configName = 'psb.config.json';
 const isCustomConfigExist = fs.existsSync(configName);
 
 if (isCustomConfigExist) {
     const workingDir = process.env.PWD;
     config = require(path.join(workingDir, configName));
+    config = merge(defaultConfig, config);
 } else {
     config = defaultConfig;
 }
